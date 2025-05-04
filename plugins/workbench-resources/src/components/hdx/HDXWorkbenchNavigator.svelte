cf107dea9e18401d92c2b49a650806db01fb1176
ify <ify@kibibox.com>
2025-05-04 16:51:50 +0500

    colors (no mixins)

--- plugins/workbench-resources/src/components/hdx/HDXWorkbenchNavigator.svelte
+++ plugins/workbench-resources/src/components/hdx/HDXWorkbenchNavigator.svelte
@@ -34,7 +34,7 @@ TODO:
   import { writable } from 'svelte/store'
 
   // debug/beahvior constants;
-  const hdxAlwaysExpand = false
+  const hdxAlwaysExpand = true
   const useFirstTimeShow = false
   const hdxAlwaysExpandWorkspaces = false // hdxAlwaysExpand
 
